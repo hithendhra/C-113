5e2aac071598f0329b19a6b5db04cbc7ad4b79c6
hithendhra <82403816+hithendhra@users.noreply.github.com>
2021-12-31 12:50:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 function preload()
 {}
 
+
 function setup()
 {
-    canvas=createCanvas(300,300);
-    canvas.center();
+    canvas=createCanvas(640,480);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_color="";
 }
 
+
 function draw()
-{}
+{
+
+}
+
 
 function take_snapshot()
 {
-    save('my_filter_image.png');
+    save('student_name.png');
+}
+
+
+function filter_tint()
+{   
 }
